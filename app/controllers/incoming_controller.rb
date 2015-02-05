30a21193a465db07468120182d15bf40019c2646
bart dangus <brskythomas@gmail.com>
2015-02-05 15:28:45 -0700

    ADD THIS SHIT

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -10,24 +10,24 @@ skip_before_action :verify_authenticity_token, only: [:create]
     # I can put the message-splitting and business ass-magic here
       #find the user by using params[:sender]
       
-      @user = User.find_by_email(params[:sender])
+      @user = User.find_by_email(params["sender"])
       #find the topic by using params[:subject]
-      @topic = Topic.find_by_title(params[:subject])
+      @topic = Topic.find_by_title(params["subject"])
       # assign the url to a variable after retrieving it from params["body-plain"]
 
       # Check if user is nil, if so, create and save a new user
       if @user = nil
-        user = User.new(email: params[:sender])
+        user = User.new(email: params["sender"])
         user.save!  
       end
       
       # Check if the topic is nil, if so, create and save a new topic
       if @topic = nil
-        topic = Topic.new(title: params[:subject])
+        topic = Topic.new(title: params["subject"])
         topic.save!
       end
       #Now that we're sure we have a valid user and topic, build and save a new bookmark
-      bookmark = Bookmark.new(body: params[:body])
+      bookmark = Bookmark.new(title: params["title"])
       bookmark.save!
       #assuming all went well, which it probably didn't
       head 200
